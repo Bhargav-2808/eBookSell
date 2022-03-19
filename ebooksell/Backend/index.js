aa4694870425bfae33e9f1ce38faed5707ab480b
Bhargav-2808 <bhargavvalani01@gmail.com>
2022-03-19 17:19:33 +0530

    BAckend setup done

---
+++ ebooksell/Backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT=6666;
+
+app.use(cors());
+
+
+
+
+
+
+// Database user : username ebook123 
+//             : password ebook123
+
